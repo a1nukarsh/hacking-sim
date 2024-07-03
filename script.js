c16af8bc64fe40e5523c3c075270ddaa06ef019e
Anukarsh <anukarshr@gmail.com>
2024-07-04 05:23:29 +0530

    udpate

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("Hack");
+
+let text = [
+  "Initializing Hacking...",
+  "Reading your files...",
+  "Password files detected...",
+  "Sending all passwords and personal files to server...",
+  "Cleaning up...",
+];
