ae4f1154dc04f157b05e98e85be9800d94f889d6
Anukarsh <anukarshr@gmail.com>
2024-07-04 22:16:39 +0530

    udpate

--- script.js
+++ script.js
@@ -1,26 +1,33 @@
 console.log("Hack");
 
+setInterval(()=>{
+    let last = document.body.lastElementChild
+}, 700)
+
+const addItem = async (item)=>{
+    await randomDelay()
+    let div = document.createElement("div")
+    div.innerHTML = item
+    document.body.append(div)
+}
+
 const randomDelay = ()=>{
     return new Promise((resolve, reject)=>{
         timeout = 1 + 6*Math.random()
         setTimeout(()=>{
             resolve()
-        }, timeout)
+        }, timeout*1000)
     })
 }
 
 let text = [
-  "Initializing Hacking...",
-  "Reading your files...",
-  "Password files detected...",
-  "Sending all passwords and personal files to server...",
-  "Cleaning up...",
+  "Initializing Hacking",
+  "Reading your files",
+  "Password files detected",
+  "Sending all passwords and personal files to server",
+  "Cleaning up",
 ];
 
 for (const item of text) {
-    addItem()
-}
-
-const addItem = async ()=>{
-
+    addItem(item)
 }
