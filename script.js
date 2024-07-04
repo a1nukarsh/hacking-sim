d2c3b17fc8c8a34d0dba84fe61ef940eb52f44ba
Anukarsh <anukarshr@gmail.com>
2024-07-04 21:42:13 +0530

    update

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 console.log("Hack");
 
+const randomDelay = ()=>{
+    return new Promise((resolve, reject)=>{
+        timeout = 1 + 6*Math.random()
+        setTimeout(()=>{
+            resolve()
+        }, timeout)
+    })
+}
+
 let text = [
   "Initializing Hacking...",
   "Reading your files...",
@@ -7,3 +16,11 @@ let text = [
   "Sending all passwords and personal files to server...",
   "Cleaning up...",
 ];
+
+for (const item of text) {
+    addItem()
+}
+
+const addItem = async ()=>{
+
+}
